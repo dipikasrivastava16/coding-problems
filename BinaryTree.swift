c3f5a0ecd6cae4bf79b50d049b9bc8c1a1e2e426
Deepika Srivastava <dipika.srivastava16@gmail.com>
2018-09-13 17:54:58 +1000

    Create BinaryTree.swift
    Created Binary tree with basic tree traversals

---
+++ BinaryTree.swift
@@ -0,0 +1,37 @@
+class Tree {
+    var rootNode:TreeNode
+    init(rootNode: TreeNode) {
+        self.rootNode = rootNode
+    }
+    func heightOfTree(_ root: TreeNode?) ->Int {
+        if root == nil { return 0 }
+        let leftlength = heightOfTree(root?.leftChild)
+        let rightlength = heightOfTree(root?.rightChild)
+        return max(leftlength+1, rightlength+1)
+        
+    }
+    func inOrderTraversal(_ root: TreeNode?) -> [Int] {
+        if root == nil {return []}
+        var result = [Int]()
+        result+=(inOrderTraversal(root?.leftChild))
+        result.append(root!.value)
+        result+=(inOrderTraversal(root!.rightChild))
+        return result
+    }
+    func preOrderTraversal(_ root: TreeNode?) -> [Int] {
+        if root == nil {return []}
+        var result = [Int]()
+        result.append(root!.value)
+        result+=(preOrderTraversal(root?.leftChild))
+        result+=(preOrderTraversal(root!.rightChild))
+        return result
+    }
+    func postOrderTraversal(_ root: TreeNode?) -> [Int] {
+        if root == nil {return []}
+        var result = [Int]()
+        result+=(postOrderTraversal(root?.leftChild))
+        result+=(postOrderTraversal(root!.rightChild))
+        result.append(root!.value)
+        return result
+    }
+}
